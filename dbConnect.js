abeba91692d11f60e8c36306e38f311a9b8cf2f6
alvingohhm <alvgoh@yahoo.com>
2022-01-14 13:00:37 +0800

    completed init connection to mongdb - created env file for local and Atlas - dbConnect to connect to both mongodb base on env

---
+++ dbConnect.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+// Connect to Mongodb
+const dbConnect = async (mongoURI) => {
+  try {
+    await mongoose.connect(
+      mongoURI,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+      (err) => {
+        if (err) throw err;
+        console.log("The connection with mongod is established");
+      }
+    );
+  } catch (error) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = dbConnect;
--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const dbConnect = require("./dbConnect");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+const URI = process.env.MONGOURI;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+////////////////////////////////////
+// Connect Database
+////////////////////////////////////
+dbConnect(URI);
 
 app.listen(PORT, () => {
   console.log(`
+  ⚡  Using Environment = ${process.env.NODE_ENV}
   🚀  Server is running
   🔉  Listening on port ${PORT}
 `);
