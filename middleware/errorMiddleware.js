b07366a8e491129c485d6dc3a2ec585e3381d7ca
alvingohhm <alvgoh@yahoo.com>
2022-01-17 11:40:55 +0800

    add middleware to catch error - catch error - catch unuse routes

---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,20 @@
+const errorMiddleware = {
+  notFound: (req, res, next) => {
+    console.log("here");
+    const error = new Error(`${req.originalUrl} - Not Found`);
+    res.status(404);
+    next(error);
+  },
+
+  errorHandler: (error, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+      status: "failed",
+      msg: error.message,
+      stack: process.env.NODE_ENV !== "production" ? error.stack : null,
+    });
+  },
+};
+
+module.exports = errorMiddleware;
